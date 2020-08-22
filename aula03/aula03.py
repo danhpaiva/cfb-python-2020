05ae885af44a786f057602db1109edc75ee5e78e
Paiva <danpaiva@live.com>
2020-08-22 00:12:17 -0300

    Variáveis em Python

--- aula03/aula03.py
+++ aula03/aula03.py
@@ -1,1 +1,13 @@
-#Variáveis em Python
+# Variáveis em Python
+num1 = num2 = resultado = 0
+nome = "Paiva"
+
+
+def ChamarNome():
+    global idade 
+    idade = 27
+    print(nome)
+
+
+ChamarNome()
+print(idade)
