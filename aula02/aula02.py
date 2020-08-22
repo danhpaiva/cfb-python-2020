b12d25e3442d193f93a07995e065975699a3a8d5
Paiva <danpaiva@live.com>
2020-08-22 00:04:27 -0300

    Sintaxe básica P2 e Comentários

---
+++ aula02/aula02.py
@@ -0,0 +1,17 @@
+# Curso de Python
+
+"""
+Sintaxe básica P2 
+e Comentários
+"""
+nome = "Daniel Paiva"
+curso = "Curso de Python"
+
+print(nome + " - " + curso)
+
+num1 = num2 = resultado = 0
+
+if 10 < 2:
+    print("Maior")
+    print("Aula 02")
+print("Fim")
