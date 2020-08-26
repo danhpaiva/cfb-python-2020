43192a21a1ade5e2b1e8c1571d83873ac93311f5
Paiva <danpaiva@live.com>
2020-08-26 20:58:30 -0300

    # Tipo Boolean

---
+++ aula08/aula08.py
@@ -0,0 +1,30 @@
+# Tipo Boolean
+
+aula = True
+
+print(aula)
+
+aula = 10 > 15
+
+print(aula)
+
+aula = "Daniel Paiva"
+print(bool(aula))
+
+aula = ""
+print(bool(aula))
+
+if aula:
+    print("Possui texto")
+else:
+    print("Vazio")
+
+aula = "Daniel Paiva"
+
+if aula:
+    print("Possui texto")
+else:
+    print("Vazio")
+
+aula = 0
+print(bool(aula))
