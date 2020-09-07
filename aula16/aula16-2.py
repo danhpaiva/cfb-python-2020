8b7f743ed7d1ae12da45db8ea1848b033acfdf7b
Daniel Paiva <danpaiva@live.com>
2020-09-07 19:43:52 -0300

    Cadastro de Turmas

--- aula16/aula16-2.py
+++ aula16/aula16-2.py
@@ -1,11 +1,11 @@
 import os
 os.system("cls")
 
-print("Cadastro de Turmas")
+print("Cadastro de Turmas\n")
 
 turma = [
-    [1, ["#####", "#####"]],
-    [2, ["#####", "#####"]]
+    [0, ["#####", "#####"]],
+    [0, ["#####", "#####"]]
 ]
 
 controlador = "0"
@@ -31,21 +31,35 @@ while controlador != "1":
         j = 1
         turma[i][j][k] = input("Informe o nome do " +
                                str(aluno) + "º" + " aluno: ")
-        aluno += 1
-        controlador2 += 1
-        k += 1
 
-    #Controle para sair do programa caso as duas turmas já estejam cadastradas
+        # Condição para limitar o quadrasto de apenas 2 estudantes
+        if aluno >= 2:
+            break
+        else:
+            controlador2 = int(
+                input("Gostaria de cadastrar mais um aluno? [1] Sim | [2] Não"))
+            if controlador2 == 1:
+                aluno += 1
+                k += 1
+            else:
+                codturma = 2
+                break
+    
+    print("\tCadastro Concluído!")
+
+    # Controle para sair do programa caso as duas turmas já estejam cadastradas
     if codturma == 2:
         break
     else:
-        controlador = input("[0] Continuar | [1] Sair ")
+        controlador = input(
+            "\tDigite: [0] Continuar no programa | [1] Sair do programa")
         controlador2 = 0
         aluno = 1
         codturma += 1
         i += 1
 
+os.system("cls")
 print("\n\tTurmas Cadastradas: \n")
 
 for linha, coluna in turma:
-    print("Turma: " + str(linha) + " Alunos: " + str(coluna))
+    print("Turma: " + str(linha) + "\nAlunos: " + str(coluna) + "\n")
