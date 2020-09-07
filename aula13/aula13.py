daeea10a4d618583878cb9578687fbc63ce5d0e5
Daniel Paiva <danpaiva@live.com>
2020-09-07 15:54:04 -0300

    Função Input

---
+++ aula13/aula13.py
@@ -0,0 +1,7 @@
+# Função Input
+numero1 = float(input("Informe o primeiro valor: "))
+numero2 = float(input("Informe o segundo valor: "))
+
+resultado = numero1 + numero2
+
+print("Soma dos valores: " + str(resultado))
