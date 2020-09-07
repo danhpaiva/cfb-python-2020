edb64a59c703bd2531eaf5f1a8fda04c0ccf463b
Daniel Paiva <danpaiva@live.com>
2020-09-07 15:35:57 -0300

    Loop FOR

---
+++ aula12/aula12.py
@@ -0,0 +1,18 @@
+# Loop FOR
+
+carros = ["HRV", "Golf", "Argo", "Focus", "Fit", "Fusion", "Polo"]
+
+for car in carros:
+    print(car)
+    if car == "Golf":
+        print(" VW")
+
+print("-------------------")
+for x in "CFB Cursos":
+    print(x)
+
+print("-------------------")
+for car in carros:
+    print(car)
+    if(car == "Fit"):
+        break
